071bf5053167b0660437e6b140fdd9847a39c559
Henry Dawson <henry.dawson@dphoto.com>
2012-07-25 11:40:06 +1000

    Server health testing

--- info.php
+++ info.php
@@ -1,5 +1,11 @@
 <?php 
 
-phpinfo();
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$result = exec("curl http://127.0.0.1/server-status?auto", $output);
+
+echo "<br><br>RESULT : $result";
+echo "<br><br>OUTPUT : " . print_r($output);
 
 ?>
