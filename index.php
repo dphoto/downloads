301b075727eb36d26d6614b9dacea17800beb274
Henry Dawson <henry.dawson@dphoto.com>
2012-08-21 01:30:22 +1000

    Redirect to IP

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ class Download extends Services{
 				exit();
 
 		}
-		/*
+		/
 
 		// REDIRECT NOT NEEDED NOW THAT STICKY SESSIONS ENABLED??
 
@@ -69,7 +69,7 @@ class Download extends Services{
 
 
 		}
-		*/		
+		
 
 
 		//if(!isset($user_id)) exit();
