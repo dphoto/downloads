25d9d0b56677f9cdd764d9ce0f8b5f5f72b42644
Henry Dawson <henry.dawson@dphoto.com>
2016-06-09 10:18:04 +1000

    dphoto/tasks#358 - Fix resigned url test

--- url.php
+++ url.php
@@ -13,7 +13,7 @@ use Aws\S3\Exception\S3Exception;
 
 
 $s3 = S3Client::factory(array(
-    'region'  => 'us-east-1'
+    'region'  => 'us-east-1',
 ));
 
 
@@ -26,8 +26,22 @@ $headers = array(
 );
 
 try{
-	$url = $s3->getObjectUrl( 'us.files.dphoto.com', $key, '+2 days', $headers );
-	echo "$url";
+
+
+$command = $s3->getCommand('GetObject', array(
+    'Bucket' => 'us.files.dphoto.com',
+    'Key' => $key,
+    'ResponseContentType' => 'image/jpeg',
+    'Scheme' => 'https',
+    'ResponseContentDisposition' => 'attachment; filename="test.jpg'
+));
+
+// Create a signed URL from the command object that will last for
+// 10 minutes from the current time
+$signedUrl = $command->createPresignedUrl('+2 days');
+
+	//$url = $s3->getObjectUrl( 'us.files.dphoto.com', $key, '+2 days', $headers );
+	echo "signed :: $signedUrl";
 }  catch( Exception $e ){
 	echo $e->getMessage();
 }
